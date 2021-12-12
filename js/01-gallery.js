558756835ac68c830df604ab3e51d1fa6cd9b471
Yevgeniy-M <jhorg.htk@gmail.com>
2021-12-12 14:40:42 +0200

    beginning

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const imagesMarkup = createimagesMarkup(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imageLink = document.addEventListener('click', onImageLink)
+
+
+
+gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createimagesMarkup(images) {
+    
+ return images.map(({ preview, original, description }) => {
+  return `
+    <div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+          />
+          </a>
+          </div>                 
+          `;
+    })
+    .join('');            
+  }
+
+
+
+
+function onImageLink(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "A") {
+    return
+  } else (
+   basicLightbox.create(instance).show()
+  )  
+ };
+
+
+
+
+
+
+
